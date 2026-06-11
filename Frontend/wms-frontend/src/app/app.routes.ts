4687e8d20320c4916affb14af14e631060db2350
AnuragBhardwaj18 <anuragbhardwaj1883@gmail.com>
2026-06-12 01:06:29 +0530

    Configured Angular routing

--- Frontend/wms-frontend/src/app/app.routes.ts
+++ Frontend/wms-frontend/src/app/app.routes.ts
@@ -1,3 +1,64 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './components/auth/login/login';
+import { Dashboard } from './components/dashboard/dashboard/dashboard';
+
+import { DepartmentList } from './components/departments/department-list/department-list';
+
+import { EmployeeList } from './components/employees/employee-list/employee-list';
+
+import { AttendanceList } from './components/attendance/attendance-list/attendance-list';
+
+import { LeaveList } from './components/leaves/leave-list/leave-list';
+
+import { ProjectList } from './components/projects/project-list/project-list';
+
+import { ReportList } from './components/reports/report-list/report-list';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    component: Login
+  },
+
+  {
+    path: 'dashboard',
+    component: Dashboard
+  },
+
+  {
+    path: 'departments',
+    component: DepartmentList
+  },
+
+  {
+    path: 'employees',
+    component: EmployeeList
+  },
+
+  {
+    path: 'attendance',
+    component: AttendanceList
+  },
+
+  {
+    path: 'leaves',
+    component: LeaveList
+  },
+
+  {
+    path: 'projects',
+    component: ProjectList
+  },
+
+  {
+    path: 'reports',
+    component: ReportList
+  }
+];
